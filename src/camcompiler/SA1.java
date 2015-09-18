da92563f683e720cb33b790bccc327d610bcfcdb
MFCo <marianococirio@gmail.com>
2015-09-17 23:40:48 -0300

    Fixed SA1
    Agrega el sacar el ultimo  consumido extra

--- src/camcompiler/SA1.java
+++ src/camcompiler/SA1.java
@@ -13,13 +13,16 @@ public class SA1 extends SemanticAction{
     //TRUNCA MAYORES A 15
     private static final int _LONG = 15;
     public SA1(){} 
-    public Token run(Token t, Warning w, int line){
+    public Token run(Token t, Warning w, int line, Reader r){
         String value=t.getValue();
+        SemanticAction sa = new SA4();
+        sa.run(r, t);
         if (value.length()>_LONG){
             value=value.substring(0, _LONG-1);
             t.setValue(value);
             w.addLog("truncated chain",line);
         }
+
         return t;
 
     }
